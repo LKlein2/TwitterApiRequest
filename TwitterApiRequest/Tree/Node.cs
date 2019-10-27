22aea8b928b6ffb91f7915490b5947d322cf1f69
Lucas klein <Lucas.Klein@live.com>
2019-10-27 15:18:52 -0300

    ajuste na Tree

--- TwitterApiRequest/Tree/Node.cs
+++ TwitterApiRequest/Tree/Node.cs
@@ -5,15 +5,15 @@ using TwitterApiRequest.Indexes;
 
 namespace TwitterApiRequest.Tree
 {
-    class Node
+    public class Node
     {
         public Node Left { get; set; }
         public Node Right { get; set; }
         public List<IndexTweetid> Value { get; set; } = new List<IndexTweetid>();
 
-        public Node(List<IndexTweetid> value)
+        public Node(IndexTweetid indexTweetId)
         {
-            this.Value = value;
+            this.Value.Add(indexTweetId);
         }
     }
 }
--- TwitterApiRequest/Tree/Tree.cs
+++ TwitterApiRequest/Tree/Tree.cs
@@ -1,92 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TwitterApiRequest.Indexes;
 
 namespace TwitterApiRequest.Tree
 {
     class Tree
     {
-        //private Node root { get; set; }
-        //private int cont;
+        private Node root { get; set; }
+        private int cont;
 
-        //public bool Add(int Value)
-        //{
-        //    if (root == null)
-        //    {
-        //        root = new Node(Value);
-        //        cont++;
-        //        return true;
-        //    }
-        //    else
-        //    {
-        //        return Add_Sub(root, Value);
-        //    }
-        //}
+        public bool Add(IndexTweetid Value)
+        {
+            if (root == null)
+            {
+                root = new Node(Value);
+                cont++;
+                return true;
+            }
+            else if (_comparer(root.Value[0],Value) == 0)
+            {
+                root.Value.Add(Value);
+                return true;
+            }
+            else
+            {
+                return Add_Sub(root, Value);
+            }
+        }
 
-        //private bool Add_Sub(Node Node, int Value)
-        //{
-        //    if (_comparer.Compare(Node.Value, Value) < 0)
-        //    {
-        //        if (Node.Right == null)
-        //        {
-        //            Node.Right = new Node(Value);
-        //            cont++;
-        //            return true;
-        //        }
-        //        else
-        //        {
-        //            return Add_Sub(Node.Right, Value);
-        //        }
-        //    }
-        //    else if (_comparer.Compare(Node.Value, Value) > 0)
-        //    {
-        //        if (Node.Left == null)
-        //        {
-        //            Node.Left = new Node(Value);
-        //            cont++;
-        //            return true;
-        //        }
-        //        else
-        //        {
-        //            return Add_Sub(Node.Left, Value);
-        //        }
-        //    }
-        //    else
-        //    {
-        //        return false;
-        //    }
-        //}
+        private bool Add_Sub(Node Node, IndexTweetid Value)
+        {
+            if (_comparer(Node.Value[0], Value) < 0)
+            {
+                if (Node.Right == null)
+                {
+                    Node.Right = new Node(Value);
+                    cont++;
+                    return true;
+                }
+                else
+                {
+                    return Add_Sub(Node.Right, Value);
+                }
+            }
+            else if (_comparer(Node.Value[0], Value) < 0)
+            {
+                if (Node.Left == null)
+                {
+                    Node.Left = new Node(Value);
+                    cont++;
+                    return true;
+                }
+                else
+                {
+                    return Add_Sub(Node.Left, Value);
+                }
+            }
+            else
+            {
+                Node.Value.Add(Value);
+                return true;
+            }
+        }
 
-        //public void Print()
-        //{
-        //    Print(root, 4);
-        //}
+        public void Print()
+        {
+            Print(root, 4);
+        }
 
-        //public void Print(Node p, int padding)
-        //{
-        //    if (p != null)
-        //    {
-        //        if (p.Right != null)
-        //        {
-        //            Print(p.Right, padding + 4);
-        //        }
-        //        if (padding > 0)
-        //        {
-        //            Console.Write(" ".PadLeft(padding));
-        //        }
-        //        if (p.Right != null)
-        //        {
-        //            Console.Write("/\n");
-        //            Console.Write(" ".PadLeft(padding));
-        //        }
-        //        Console.Write(p.Value.ToString() + "\n ");
-        //        if (p.Left != null)
-        //        {
-        //            Console.Write(" ".PadLeft(padding) + "\\\n");
-        //            Print(p.Left, padding + 4);
-        //        }
-        //    }
-        //}
+        public void Print(Node p, int padding)
+        {
+            if (p != null)
+            {
+                if (p.Right != null)
+                {
+                    Print(p.Right, padding + 4);
+                }
+                if (padding > 0)
+                {
+                    Console.Write(" ".PadLeft(padding));
+                }
+                if (p.Right != null)
+                {
+                    Console.Write("/\n");
+                    Console.Write(" ".PadLeft(padding));
+                }
+                Console.Write(p.Value.ToString() + "\n ");
+                if (p.Left != null)
+                {
+                    Console.Write(" ".PadLeft(padding) + "\\\n");
+                    Print(p.Left, padding + 4);
+                }
+            }
+        }
+
+        public static int _comparer(IndexTweetid id1, IndexTweetid id2)
+        {
+            return String.Compare(id1.GetKey().ToUpper().Trim(), id2.GetKey().ToUpper().Trim(), StringComparison.Ordinal);
+        }
 
     }
 }
